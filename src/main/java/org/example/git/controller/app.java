235c2ca27669eee52df9fa905da6bdb9c40a1d18
Almas <96418924+almass1223334444@users.noreply.github.com>
2026-01-16 11:48:18 +0500

    sdfsdfsd

--- src/main/java/org/example/git/controller/app.java
+++ src/main/java/org/example/git/controller/app.java
@@ -4,5 +4,8 @@ public class app {
     public static void main(String[] args){
         int num = 4;
         System.out.println(num);
+        if (num > 3){
+            System.out.println(1);
+        }
     }
 }
